761e0364a671d4a4b902c0aa9e793240161d2c26
petsereypanha <sereypanhapet@gmail.com>
2025-09-02 14:55:10 +0700

    add rxjs operators

--- src/app/features/products/components/product-list.component.ts
+++ src/app/features/products/components/product-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {from, fromEvent, of, Subscription} from 'rxjs';
+import {filter, from, fromEvent, map, of, Subscription, take, tap, timer} from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -15,20 +15,23 @@ export class ProductListComponent implements OnInit, OnDestroy{
     subString!: Subscription;
     subEvent!: Subscription;
     subKey!: Subscription;
+    subApps!: Subscription;
+    subFilter!: Subscription;
+    subTimer!: Subscription;
 
   ngOnInit(): void {
-    // this.sub = of(1, 2, 3).subscribe(item => console.log(`Number: ${item}`));
-    // this.subArray = of([1, 2, 3]).subscribe(item => console.log(`Array: ${item}`));
-    // this.subFrom   = from([1, 2, 3]).subscribe({
-    //   next: item => console.log(`From: ${item}`),
-    //   error: err => console.log('From error: ', err),
-    //   complete: () => console.log('From completed')
-    // });
-    // this.subString = of('Hello2', 'Hello1').subscribe({
-    //   next: item => console.log(`String: ${item}`),
-    //   error: err => console.log('String error: ', err),
-    //   complete: () => console.log('String completed')
-    // });
+    this.sub = of(1, 2, 3).subscribe(item => console.log(`Number: ${item}`));
+    this.subArray = of([1, 2, 3]).subscribe(item => console.log(`Array: ${item}`));
+    this.subFrom   = from([1, 2, 3]).subscribe({
+      next: item => console.log(`From: ${item}`),
+      error: err => console.log('From error: ', err),
+      complete: () => console.log('From completed')
+    });
+    this.subString = of('Hello2', 'Hello1').subscribe({
+      next: item => console.log(`String: ${item}`),
+      error: err => console.log('String error: ', err),
+      complete: () => console.log('String completed')
+    });
     this.subEvent = fromEvent(document, 'click').subscribe({
       next: ev => console.log(`Event: ${ev.target}`),
       error: err => console.log('Event error: ', err),
@@ -41,6 +44,32 @@ export class ProductListComponent implements OnInit, OnDestroy{
         console.log(`Key: ${key}`);
       }
     );
+
+    const apps$ = from([
+      {name: 'App1', version: '1.0.0'},
+      {name: 'App2', version: '1.0.1'},
+      {name: 'App3', version: '1.0.2'},
+    ])
+    this.subApps = apps$
+      .pipe(
+        map(app => ({...app, version: app.version + '.0'})),
+        tap( app => console.log(`App: ${app.name}, Version: ${app.version}`))
+      )
+      .subscribe();
+    this.subFilter = of(2, 3 ,  4, 5, 6).pipe(
+      filter(app => app % 2 === 0),
+      tap(x => console.log(`Filter: ${x}`))
+    ).subscribe();
+
+    this.subTimer = timer(0, 1000)
+      .pipe(
+        take(5)
+      )
+      .subscribe({
+      next: val => console.log(`Timer: ${val}`),
+      error: err => console.log('Timer error: ', err),
+      complete: () => console.log('Timer completed')
+    })
   }
 
   ngOnDestroy(): void {
@@ -49,5 +78,9 @@ export class ProductListComponent implements OnInit, OnDestroy{
     this.subFrom.unsubscribe();
     this.subString.unsubscribe();
     this.subEvent.unsubscribe();
+    this.subKey.unsubscribe();
+    this.subApps.unsubscribe();
+    this.subFilter.unsubscribe();
+    this.subTimer.unsubscribe();
   }
 }
