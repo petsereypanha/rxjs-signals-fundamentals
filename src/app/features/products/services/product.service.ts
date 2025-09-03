aecfb7aabfb63ee65464a50325cdf59c93ae4b3a
petsereypanha <sereypanhapet@gmail.com>
2025-09-03 12:47:12 +0700

    add related data with switchMap

--- src/app/features/products/services/product.service.ts
+++ src/app/features/products/services/product.service.ts
@@ -1,9 +1,10 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError, Observable, of, tap, throwError} from 'rxjs';
+import {catchError, map, Observable, of, switchMap, tap, throwError} from 'rxjs';
 import {Product} from '../product';
 import {ProductData} from '../product-data';
 import {HttpErrorService} from '../../../utilities/http-error.service';
+import {ReviewService} from '../../reviews/review.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient);
   private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -26,8 +28,20 @@ export class ProductService {
     return this.http.get<Product>(url)
       .pipe(
         tap(data => console.info('In http.get by id pipeline')),
+        switchMap(product => this.getProductsWithReviews(product)),
         catchError(err => this.handleError(err))
       );
+  }
+
+  private getProductsWithReviews(product: Product):  Observable<Product> {
+      if (product.hasReviews) {
+        return this.http.get<ProductData[]>(this.reviewService.getReviewUrl(product.id))
+          .pipe(
+            map(reviews => ({...product, reviews} as Product)),
+          );
+      } else {
+        return of(product);
+      }
   }
 
   private handleError(err:HttpErrorResponse): Observable<never> {
